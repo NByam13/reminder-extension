0eca8848200c8ed235c8ab45944c0ea1bdde6e7c
Vehikl Team <go@vehikl.com>
2021-11-19 16:46:52 -0500

    add alarm list

--- background.js
+++ background.js
@@ -1,89 +1,96 @@
 // on install of the extension, check to see if checked has been set
 // if it has then do nothing, other wise set it to false
 chrome.runtime.onInstalled.addListener(() => {
-    // on install, check to see if there is any existing data which is not needed
-    chrome.storage.sync.get(['date', 'time', 'repeat', 'msg'], ({ date, time, repeat, msg }) => {
-        if (date || time || repeat || msg) {
-            // remove storage keys if they exist
-            chrome.storage.sync.remove(['date', 'time', 'repeat', 'msg']);
-        }
+  // on install, check to see if there is any existing data which is not needed
+  chrome.storage.sync.get(
+    ["date", "time", "repeat", "msg"],
+    ({ date, time, repeat, msg }) => {
+      if (date || time || repeat || msg) {
+        // remove storage keys if they exist
+        chrome.storage.sync.remove(["date", "time", "repeat", "msg"]);
+      }
 
-        // reset storage keys to defaults
-        chrome.storage.sync.set({ date: '' })
-        chrome.storage.sync.set({ time: '' })
-        chrome.storage.sync.set({ repeat: false })
-        chrome.storage.sync.set({ msg: '' })
-    })
-})
+      // reset storage keys to defaults
+      chrome.storage.sync.set({ date: "" });
+      chrome.storage.sync.set({ time: "" });
+      chrome.storage.sync.set({ repeat: false });
+      chrome.storage.sync.set({ msg: "" });
+    }
+  );
+});
 
 // add a listener for browser messages coming from popup.js
 chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
-    let success = false
+  let success = false;
 
-    // grab the date and time from the request
-    const { date, time } = request
+  // grab the date and time from the request
+  const { date, time } = request;
 
-    // create a new date object from the date and time
-    const reminderDate = new Date(`${date} ${time}`)
-    const when = reminderDate.getTime()
+  // create a new date object from the date and time
+  const reminderDate = new Date(`${date} ${time}`);
+  const when = reminderDate.getTime();
 
-    // make sure the time is after the current time
-    if (when > Date.now()) {
-        // create alarmInfo config object
-        const alarmInfo = { when: when }
+  // make sure the time is after the current time
+  if (when > Date.now()) {
+    // create alarmInfo config object
+    const alarmInfo = { when: when };
 
-        // set a new alarm for the date object
-        chrome.alarms.create('reminder', alarmInfo)
-        success = true
-    }
+    // set a new alarm for the date object
+    chrome.alarms.create(`reminder-${Date.now()}`, alarmInfo);
+    success = true;
+  }
 
-    // send a response to the popup.js that the alarm has been set
-    sendResponse({ success })
-})
+  // send a response to the popup.js that the alarm has been set
+  sendResponse({ success });
+});
 
 // create a listener for alarms
 chrome.alarms.onAlarm.addListener(() => {
-    let status = 'You set a reminder for this time!'
+  let status = "You set a reminder for this time!";
 
-    chrome.storage.sync.get(['msg'], ({ msg }) => {
-        if (msg.length > 0) {
-            status = msg
+  chrome.storage.sync.get(["msg"], ({ msg }) => {
+    if (msg.length > 0) {
+      status = msg;
+    }
+
+    chrome.notifications.create(
+      `reminder-${Date.now()}`,
+      {
+        title: "Reminder!",
+        message: status,
+        priority: 2,
+        type: "basic",
+        iconUrl: "/images/get_started48.png",
+      },
+      () => {
+        console.log("notification created");
+      }
+    );
+  });
+
+  // check if repeat is clicked or not and set the alarm accordingly
+  chrome.storage.sync.get(["repeat"], ({ repeat }) => {
+    console.log("repeat: " + repeat);
+
+    if (repeat) {
+      // re-set the alarm for a time period of 1 minute, TODO: make repeat time configurable
+      const reminderDate = new Date(Date.now() + 60 * 1000); // adding 1m for testing's sake
+      const when = reminderDate.getTime();
+      let repeatMsg = `A reminder was created for ${reminderDate} with message: ${status}`;
+      chrome.alarms.create("reminder", { when: when });
+      chrome.notifications.create(
+        `repeat-reminder-${Date.now()}`,
+        {
+          title: "Reminder Created",
+          message: repeatMsg,
+          priority: 2,
+          type: "basic",
+          iconUrl: "/images/get_started48.png",
+        },
+        () => {
+          console.log("notification created");
         }
-
-        chrome.notifications.create(`reminder-${Date.now()}`,
-            {
-                title: 'Reminder!',
-                message: status,
-                priority: 2,
-                type: 'basic',
-                iconUrl: "/images/get_started48.png"
-            }, () => {
-                console.log('notification created')
-            })
-    })
-
-    // check if repeat is clicked or not and set the alarm accordingly
-    chrome.storage.sync.get(['repeat'], ({ repeat }) => {
-        console.log('repeat: ' + repeat)
-
-        if (repeat) {
-            // re-set the alarm for a time period of 1 minute, TODO: make repeat time configurable
-            const reminderDate = new Date(Date.now() + (60 * 1000)) // adding 1m for testing's sake 
-            const when = reminderDate.getTime()
-            let repeatMsg = `A reminder was created for ${reminderDate} with message: ${status}`
-            chrome.alarms.create('reminder', { when: when })
-            chrome.notifications.create(`repeat-reminder-${Date.now()}`,
-                {
-                    title: 'Reminder Created',
-                    message: repeatMsg,
-                    priority: 2,
-                    type: 'basic',
-                    iconUrl: "/images/get_started48.png"
-                }, () => {
-                    console.log('notification created')
-                })
-        }
-    })
-})
-
-
+      );
+    }
+  });
+});
--- popup.js
+++ popup.js
@@ -1,100 +1,135 @@
-const checkbox = document.getElementById('repeat-chkbx')
-const dateIn = document.getElementById('date-input')
-const timeIn = document.getElementById('time-input')
-const setBtn = document.getElementById('set-rmndr-btn')
-const msgBox = document.getElementById('msg-box')
+const checkbox = document.getElementById("repeat-chkbx");
+const dateIn = document.getElementById("date-input");
+const timeIn = document.getElementById("time-input");
+const setBtn = document.getElementById("set-rmndr-btn");
+const msgBox = document.getElementById("msg-box");
+
+window.onload = function () {
+  updateAlarmList();
+};
+
+const updateAlarmList = () => {
+  chrome.alarms.getAll((alarms) => {
+    let alarmList = "<ul>";
+    alarms
+      .sort((first, second) => first.scheduledTime - second.scheduledTime)
+      .forEach((element) => {
+        alarmList += `<li>${new Date(
+          element.scheduledTime
+        ).toLocaleString()}</li>`;
+      });
+    alarmList += "</ul>";
+
+    document.getElementById("alarm-list").innerHTML = alarmList;
+  });
+};
 
 // grab the stored value for repeat, let the other values reset when popup is closed
-chrome.storage.sync.get(['repeat'], ({ repeat }) => {
-    checkbox.checked = repeat
-})
+chrome.storage.sync.get(["repeat"], ({ repeat }) => {
+  checkbox.checked = repeat;
+});
 
 // look for blur event because it's the only way to know when the user has finished selecting the date
-dateIn.addEventListener('blur', (event) => {
-    const date = event.target.value
-    chrome.storage.sync.set({ date }, () => {
-        console.log('date saved')
-    })
-})
+dateIn.addEventListener("blur", (event) => {
+  const date = event.target.value;
+  chrome.storage.sync.set({ date }, () => {
+    console.log("date saved");
+  });
+});
 
 // look for blur event because it's the only way to know when the user has finished selecting the time
-timeIn.addEventListener('blur', (event) => {
-    const time = event.target.value
-    chrome.storage.sync.set({ time }, () => {
-        console.log('time saved')
-    })
-})
+timeIn.addEventListener("blur", (event) => {
+  const time = event.target.value;
+  chrome.storage.sync.set({ time }, () => {
+    console.log("time saved");
+  });
+});
 
-checkbox.addEventListener('change', (event) => {
-    const repeat = event.target.checked
-    chrome.storage.sync.set({ repeat }, () => {
-        console.log('repeat saved')
-    })
-})
+checkbox.addEventListener("change", (event) => {
+  const repeat = event.target.checked;
+  chrome.storage.sync.set({ repeat }, () => {
+    console.log("repeat saved");
+  });
+});
 
 // listen for blur event so we know when text area loses focus
-msgBox.addEventListener('blur', (event) => {
-    const msg = event.target.value
-    chrome.storage.sync.set({ msg }, () => {
-        console.log('msg saved')
-    })
-})
+msgBox.addEventListener("blur", (event) => {
+  const msg = event.target.value;
+  chrome.storage.sync.set({ msg }, () => {
+    console.log("msg saved");
+  });
+});
 
-setBtn.addEventListener('click', () => {
-    // make sure the user has selected a date and time first
+setBtn.addEventListener("click", () => {
+  // make sure the user has selected a date and time first
 
-    if (!dateIn.value || !timeIn.value) {
-        chrome.notifications.create(`reminder-failed-${Date.now()}`,
-            {
-                title: 'Reminder Failed',
-                message: "Please enter a date and time first to set a reminder",
-                priority: 2,
-                type: 'basic',
-                iconUrl: "/images/get_started48.png"
-            }, () => {
-                console.log('notification failed')
-            })
-        return false;
+  if (!dateIn.value || !timeIn.value) {
+    chrome.notifications.create(
+      `reminder-failed-${Date.now()}`,
+      {
+        title: "Reminder Failed",
+        message: "Please enter a date and time first to set a reminder",
+        priority: 2,
+        type: "basic",
+        iconUrl: "/images/get_started48.png",
+      },
+      () => {
+        console.log("notification failed");
+      }
+    );
+    return false;
+  }
+
+  // make sure reminder date and time is in the future
+  const date = new Date(`${dateIn.value} ${timeIn.value}`);
+
+  if (date.getTime() < Date.now()) {
+    chrome.notifications.create(
+      `reminder-failed-${Date.now()}`,
+      {
+        title: "Reminder Failed",
+        message: "I can't remind you in the past... yet.",
+        priority: 2,
+        type: "basic",
+        iconUrl: "/images/get_started48.png",
+      },
+      () => {
+        console.log("notification failed");
+      }
+    );
+    return false;
+  }
+
+  // fetch results from storage api
+  chrome.storage.sync.get(
+    ["date", "time", "repeat", "msg"],
+    ({ date, time, repeat, msg }) => {
+      //then tell background.js about the new reminder to create, and wait for the response
+      chrome.runtime.sendMessage({ date, time, repeat, msg }, ({ success }) => {
+        // check the response from background.js and create a notification on the outcome.
+        let status = `A reminder has been created for ${date} ${time}`;
+
+        if (msg.length > 0) {
+          status += ` with message: ${msg}`;
+        }
+
+        chrome.notifications.create(
+          `reminder-created-${Date.now()}`,
+          {
+            title: "Reminder Created",
+            message: status,
+            priority: 2,
+            type: "basic",
+            iconUrl: "/images/get_started48.png",
+          },
+          () => {
+            console.log("notification created");
+          }
+        );
+        if (success) {
+          updateAlarmList();
+        }
+      });
     }
-
-    // make sure reminder date and time is in the future
-    const date = new Date(`${dateIn.value} ${timeIn.value}`)
-
-    if (date.getTime() < Date.now()) {
-        chrome.notifications.create(`reminder-failed-${Date.now()}`,
-            {
-                title: 'Reminder Failed',
-                message: "I can't remind you in the past... yet.",
-                priority: 2,
-                type: 'basic',
-                iconUrl: "/images/get_started48.png"
-            }, () => {
-                console.log('notification failed')
-            })
-        return false
-    }
-
-    // fetch results from storage api
-    chrome.storage.sync.get(['date', 'time', 'repeat', 'msg'], ({ date, time, repeat, msg }) => {
-        //then tell background.js about the new reminder to create, and wait for the response
-        chrome.runtime.sendMessage({ date, time, repeat, msg }, () => {
-            // check the response from background.js and create a notification on the outcome.
-            let status = `A reminder has been created for ${date} ${time}`
-
-            if (msg.length > 0) {
-                status += ` with message: ${msg}`
-            }
-
-            chrome.notifications.create(`reminder-created-${Date.now()}`,
-                {
-                    title: 'Reminder Created',
-                    message: status,
-                    priority: 2,
-                    type: 'basic',
-                    iconUrl: "/images/get_started48.png"
-                }, () => {
-                    console.log('notification created')
-                })
-        })
-    })
-})
+  );
+});
